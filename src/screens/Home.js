e855a0d3e59efbf3deb6f7491410ade65983f493
Antonio Massaro <tolf96@gmail.com>
2024-02-04 15:21:37 +0100

    Login page IN PROGRESS

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Home = () => {
+    const clearAsyncStorage = function() {
+        AsyncStorage.clear();
+    }
+
     return (
         <View>
             <Text>Home</Text>
+            <Button title="Clear AsyncStorage" onPress={clearAsyncStorage} />
         </View>
     );
 }
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,88 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet, ImageBackground, TextInput, TouchableOpacity } from "react-native";
+import { auth } from "../config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import BackgroundImage from "../../assets/wheel_icon.png";
+import CustomText from "../components/CustomText";
+import CustomInput from "../components/CustomInput";
 
 const Login = () => {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const handleLoginPress = function() {
+        createUserWithEmailAndPassword(auth, "tolf96@gmail.com", "sium96").then(userCredential => {
+            console.log(userCredential);
+        }).catch(error => {
+            console.error(error);
+        });
+    };
+
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageBackground resizeMode="cover" source={BackgroundImage} style={styles.image}>
+                </ImageBackground>
+            </View>
+            <View style={styles.formContainer}>
+                <View style={styles.inputBox}>
+                    <CustomInput
+                        style={styles.input}
+                        placeholder="Username"
+                        value={username}
+                        onChangeText={(text) => setUsername(text)}
+                    />
+                    <CustomInput
+                        style={styles.input}
+                        placeholder="Password"
+                        secureTextEntry={true}
+                        value={password}
+                        onChangeText={(text) => setPassword(text)}
+                    />
+                </View>
+
+                <TouchableOpacity>
+                    <CustomText>Login</CustomText>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "white"
+    },
+    imageContainer: {
+        flex: 1,
+        alignItems: "center",
+        paddingHorizontal: 50
+    },
+    image: {
+        flex: 1,
+        width: 500
+    },
+    formContainer: {
+        borderColor: "grey",
+        borderWidth: 3,
+        flex: 1.5,
+        borderTopLeftRadius: 50,
+        borderTopRightRadius: 50,
+        paddingHorizontal: 30
+    },
+    input: {
+        height: 60,
+        width: '100%',
+        borderColor: 'gray',
+        borderWidth: 2,
+        marginBottom: 16,
+        padding: 8,
+        borderRadius: 10
+    },
+    inputBox: {
+        marginTop: 120
+    }
+});
 
 export default Login;
